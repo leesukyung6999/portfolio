3f67a39dee2c5b4d2e4d212deb3315dc3f0dbd1f
leesukyung6999 <sukyung699@naver.com>
2021-07-22 09:56:46 +0900

    7/22
    제어전

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,30 +1,19 @@
 $(document).ready(function(){
-    // $(window).on('scroll', function(){
-    //     // 1) #banner #intro에서는 사라지게 
-    //     // 2) #intro parallax
-    //     const scrollY =$(this).scrollTop();
-    //     if (scrollY < $('#intro').height()) {
-    //         $('#banner').hide();
-    //         $('#intro .intro').addClass('on');
-    //     }
-    //     else $('#banner').show();
-    // });
-    // $(window).trigger('scroll');
+    $(window).on('scroll',function(){
+        // 1) intro fade 효과로 올라오기
+        let scrollY = $(this).scrollTop() + $(this).height() * 2/3;
+        $('.intro').each(function(){
+            if (scrollY > $(this).offset().top) {
+                $(this).addClass('fade');
+            }
+            else {
+                $(this).removeClass('fade');
+            }
+        });
 
-    // 3) #about 클릭 이벤트
-    const $about = $('#about');
-    // 3-1) 접근성: .txt => aria-hidden=true
-    $about.find('.txt').attr('aria-hidden',true);
-    $about.find('.open').on('click',function(){
-        const wrapHei = $('#wrap').height();
-        //console.log(typeof wrapHei);
-        $('html, body').css({height: wrapHei, overflow: 'hidden'});
-        $about.addClass('on').siblings('#banner').hide();
-        // 3-2) 접근성: .window => aria-hidden: true / .txt => aria-hidden: false
-        $about.find('.txt').attr('aria-hidden',false).siblings().attr('aria-hidden',true);
+
     });
-    $about.find('.go_back').on('click',function(){
-        $('html, body').removeAttr('style');
-        $about.removeClass('on').siblings('#banner').show();
-    });
+    $(window).trigger('scroll');
+
+    
 });
